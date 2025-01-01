2131c4c51824a4349cb0eb427b369a5a0a25d5a2
Parth Antala <theparthantala@gmail.com>
2025-01-01 12:26:39 -0500

    Initial Commit

---
+++ Happy New Year/Happy_New_YearApp.swift
@@ -0,0 +1,17 @@
+//
+//  Happy_New_YearApp.swift
+//  Happy New Year
+//
+//  Created by Parth Antala on 2025-01-01.
+//
+
+import SwiftUI
+
+@main
+struct Happy_New_YearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
